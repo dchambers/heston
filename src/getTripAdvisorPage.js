cd2cec977b7ea3adc4650a1d9301f5b861fcb89c
dchambers <dominic.chambers@gmail.com>
2016-04-28 21:50:29 +0100

    Use Google API to get TripAdvisor page
    The Google API is better suited to free text entry so works better.

--- src/getTripAdvisorPage.js
+++ src/getTripAdvisorPage.js
@@ -1,28 +1,11 @@
 import fetch from 'node-fetch';
 import urlencode from 'urlencode';
-import htmlparser from "htmlparser2";
+import companyData from '../companyData';
+
+const API_KEY = companyData.googleApiKey;
+const SEARCH_ENGINE = companyData.googleSearchKey;
 
 export default function getTripAdvisorPage(restaurant) {
-	return fetch('https://www.tripadvisor.co.uk/Search?q=' + urlencode(restaurant))
-		.then(function(res) {
-			return res.text();
-		})
-		.then(function(body) {
-			var restaurantPageUrl;
-			var parser = new htmlparser.Parser({
-				onopentag: function(name, attribs) {
-					if(name === 'div' && attribs.class === 'title' && attribs.onclick) {
-						if(!restaurantPageUrl) {
-							var restaurantPageUrlData = attribs.onclick.split(' ')[4];
-							restaurantPageUrl = 'https://www.tripadvisor.co.uk' +
-								restaurantPageUrlData.substring(1, restaurantPageUrlData.length - 2);
-						}
-					}
-				}
-			}, {decodeEntities: true});
-			parser.write(body);
-			parser.end();
-
-			return restaurantPageUrl;
-		});
+  const url = `https://www.googleapis.com/customsearch/v1?q=${urlencode(restaurant)}&cx=${SEARCH_ENGINE}&key=${API_KEY}`;
+  return fetch(url).then(res => res.json()).then(json => json.items[0].link);
 }
