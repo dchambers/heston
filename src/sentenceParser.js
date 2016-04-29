c973e2063d1537a40f2de84938c0e7477014555b
dchambers <dominic.chambers@gmail.com>
2016-04-29 22:15:09 +0100

    Add review sentence parser

---
+++ src/sentenceParser.js
@@ -0,0 +1,48 @@
+import {zipObj} from 'ramda';
+import pluralize from 'pluralize';
+import companyData from '../companyData'
+
+export const DEFAULT_LOCATION = companyData.companyAddress;
+
+export const FOOT = 'FOOT';
+export const PUBLIC_TRANSPORT = 'PUBLIC_TRANSPORT';
+
+export const LOW = 'LOW';
+export const HIGH = 'HIGH';
+
+export const findWordGroups = (sentence) => {
+  return sentence.replace('to eat', 'to_eat').replace('get food', 'get_food');
+};
+
+export const sentenceParser = (sentence) => {
+  const isQuestion = sentence.substr(sentence.length - 1, 1) == '?';
+
+  if(isQuestion) {
+    const sentenceWords = sentence.substr(0, sentence.length - 1);
+    const wordList = findWordGroups(sentenceWords).split(/[ ]+/).map(pluralize.singular);
+    const words = zipObj(wordList, wordList.map(() => true));
+    const restaurantQuery = (words.restaurant || words.meal || words.to_eat);
+    const lunchQuery = (words.lunch || words.lunchtime || words.get_food);
+
+    if((restaurantQuery || lunchQuery) && (words.trying || words.good || words.nice || words.favourite ||
+      words.best || words.recommend || words.recommendation)) {
+      const locationMatches = sentence.match(/(in|near) (.*)\?/);
+      const rawLocation = (locationMatches) ? locationMatches[2] : DEFAULT_LOCATION;
+      const location = rawLocation.replace(/(here|town|the city|the office)/i, DEFAULT_LOCATION);
+      const foodQuery = (restaurantQuery) ?
+        {
+          near: location,
+          by: PUBLIC_TRANSPORT
+        } :
+        {
+          near: location,
+          by: FOOT,
+          price: LOW
+        };
+
+      return (words.client || words.business) ? {...foodQuery, price: HIGH} : foodQuery;
+    }
+  }
+
+  return null;
+};
---
+++ src/sentenceParser.spec.js
@@ -0,0 +1,79 @@
+import {describe, it} from 'mocha';
+import {expect} from 'chai';
+import {sentenceParser as parsedSentence, DEFAULT_LOCATION, FOOT, PUBLIC_TRANSPORT, LOW, HIGH} from './sentenceParser';
+
+describe('sentenceParser', () => {
+	describe('restaurant recommendations', () => {
+		const restaurant = {
+			near: DEFAULT_LOCATION,
+			by: PUBLIC_TRANSPORT
+		};
+
+		it('recommends all prices of restaurant if asked for a general restaurant recommendation.', () => {
+			expect(parsedSentence('Which restaurants should I be trying while I\'m in town?')).to.deep.equal(restaurant);
+			expect(parsedSentence('Are there any good restaurants around here that people can recommend?')).to.deep.equal(restaurant);
+			expect(parsedSentence('What are your favourite places to eat in New York?')).to.deep.equal({
+				...restaurant,
+				near: 'New York'
+			});
+		});
+
+		it.skip('only recommends local restaurants if given a specific place.', () => {
+			expect(parsedSentence('Any suggestions for restaurants near Euston St Pancras?')).to.deep.equal({
+				near: 'Euston St Pancras',
+				by: FOOT
+			});
+		});
+
+		it('does not make recommendations when people are just talking about places they ate at.', () => {
+			expect(parsedSentence('I had a really nice meal last night!')).to.be.null;
+			expect(parsedSentence('Did you end up going to that restaurant I suggested yesterday?')).to.be.null;
+			expect(parsedSentence('Did you end up going to that restaurant I suggested yesterday?')).to.be.null;
+		});
+	});
+
+	describe('lunch recommendations', () => {
+		const lunch = {
+			price: LOW,
+			near: DEFAULT_LOCATION,
+			by: FOOT
+		};
+
+		it('recommends good value places within walking distance for lunch.', () => {
+			expect(parsedSentence('Where are the best places to get food near the office?')).to.deep.equal(lunch);
+			expect(parsedSentence('Where\'s good for lunch?')).to.deep.equal(lunch);
+			expect(parsedSentence('Any recommendations for lunch near here?')).to.deep.equal(lunch);
+		});
+
+		it('does not make recommendations when people are just talking about their lunch.', () => {
+			expect(parsedSentence('Lunch was great today!')).to.be.null;
+			expect(parsedSentence('Are people ready to go for lunch?')).to.be.null;
+			expect(parsedSentence('It\'s lunch time people!')).to.be.null;
+		});
+	});
+
+	describe('client meals', () => {
+		const clientMeal = {
+			price: HIGH,
+			near: 'London',
+			by: PUBLIC_TRANSPORT
+		};
+
+		it('Recommends pricey restaurants for client meals', () => {
+			expect(parsedSentence('Can anybody recommend a good restaurant to take a client in London?')).to.deep.equal(clientMeal);
+		});
+	});
+
+	describe('client lunches', () => {
+		const clientLunch = {
+			price: HIGH,
+			near: DEFAULT_LOCATION,
+			by: FOOT
+		};
+
+		it('Recommends pricey but local restaurants for business lunches', () => {
+			expect(parsedSentence('Where is good for a business lunch?')).to.deep.equal(clientLunch);
+			expect(parsedSentence('Where would people recommend if I need to take a client for lunch?')).to.deep.equal(clientLunch);
+		});
+	});
+});
