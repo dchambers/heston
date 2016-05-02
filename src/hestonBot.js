9c20cc19fe9fe8892659f7133c5ea7a5b63a0264
dchambers <dominic.chambers@gmail.com>
2016-05-02 22:40:51 +0100

    Make review data available for querying
    Testing this required the tests to be rewritten to use explicit `before`
    blocks, since these are only run immediately before the tests they
    contain, allowing the created within these blocks to be resolved before
    the next inner test block code is evaluated.

--- src/hestonBot.js
+++ src/hestonBot.js
@@ -1,13 +1,23 @@
 /* eslint "no-implicit-side-effects/no-implicit-side-effects": "error" */
-import {assoc, assocPath} from 'ramda';
+import {assoc, assocPath, dissoc} from 'ramda';
+import {sentenceParser, FOOT} from './sentenceParser';
+import {getTravelDuration} from './utils';
+import companyData from '../companyData';
 
 const AWAITING_RESTAURANT_CONFIRMATION = 1;
 const AWAITING_RATING = 2;
 const AWAITING_REVIEW = 3;
-const COMPLETE = 3;
 
 const action = (state, messages) => ({state, messages});
 const userMessage = (user, message) => ({type:'USER', user, message});
+
+const removeStateProp = dissoc('state');
+
+const createReviewFilter = (filter) => (restaurant) => {
+	const travelDuration = getTravelDuration(filter.near, restaurant.location, (filter.by == FOOT) ? 'walking' : 'driving');
+	const priceMatch = true; // TODO: we need to add pricing information to the restaurant data blob
+	return (travelDuration < (filter.by == FOOT) ? 10 : 30) && priceMatch;
+};
 
 const hestonBot = (state = {users: {}, reviews: []}, message, data) => {
 	if(message.match(/^heston review /)) {
@@ -17,11 +27,23 @@ const hestonBot = (state = {users: {}, reviews: []}, message, data) => {
 
 		return action(updatedState, [
 			userMessage(data.user.name, data.getPlaceInfo(restaurant).then(placeInfo => {
-				void (data.placeInfo = placeInfo);
+				void (updatedState.users[data.user.id].placeInfo = placeInfo);
 				return placeInfo.tripAdvisorLink;
 			})),
 			userMessage(data.user.name, 'Is this the restaurant you want to review?')
 		]);
+	}
+	else {
+		const parsedSentence = sentenceParser(message);
+
+		if(parsedSentence) {
+			const reviewFilter = createReviewFilter(parsedSentence);
+			const qualifyingRestaurants = state.reviews.filter(reviewFilter);
+
+			return action(state, [
+				userMessage(data.user.name, `I have ${qualifyingRestaurants.length} recommendations for restaurants near ${reviewFilter.near} from other ${companyData.companyName} staff if you're interested?\nType 'ok @heston' to see them.`)
+			]);
+		}
 	}
 
 	const userState = state.users[data.user.id] || {};
@@ -43,7 +65,7 @@ const hestonBot = (state = {users: {}, reviews: []}, message, data) => {
 
 		case AWAITING_RATING: {
 			if(message.match(/[0-5]/)) {
-				const updatedState = assocPath(['users', data.user.id], {...userState, rating: message, state: AWAITING_REVIEW}, state);
+				const updatedState = assocPath(['users', data.user.id], {...userState, rating: Number(message), state: AWAITING_REVIEW}, state);
 				return action(updatedState, [
 					userMessage(data.user.name, 'Thanks! Can you please provide some textual content to justify that rating.')
 				]);
@@ -57,9 +79,10 @@ const hestonBot = (state = {users: {}, reviews: []}, message, data) => {
 		}
 
 		case AWAITING_REVIEW: {
-			const restaurantReview = {...userState, user: data.user.name, description: message, state: COMPLETE};
+			const restaurantReview = removeStateProp({...userState, user: data.user.name, description: message});
 			const updatedState = assocPath(['users', data.user.id], {}, state);
 			const updatedState2 = assoc('reviews', [...updatedState.reviews, restaurantReview], updatedState);
+
 			return action(updatedState2, [
 				userMessage(data.user.name, 'Your review has been noted my friend!')
 			]);
--- src/hestonBot.spec.js
+++ src/hestonBot.spec.js
@@ -1,4 +1,4 @@
-import {describe, it} from 'mocha';
+import {describe, it, before} from 'mocha';
 import chai, {expect} from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import hestonBot from './hestonBot';
@@ -7,16 +7,20 @@ chai.use(chaiAsPromised);
 
 const user = {id: 1, name: 'Fred'};
 const getPlaceInfo = restaurant => Promise.resolve({tripAdvisorLink: '@' + restaurant});
-const data = {user, getPlaceInfo};
+const data = () => ({user, getPlaceInfo});
 
 describe('conversation with Heston', () => {
 	it('ignores conversations unless they start as expected', () => {
-		const result = hestonBot(undefined, 'Yo!', data);
+		const result = hestonBot(undefined, 'Yo!', data());
 		expect(result.messages.length).to.equal(0);
 	});
 
 	describe('conversation after a restaurant is suggested', () => {
-		const suggestionResult = hestonBot(undefined, 'heston review Krusty Burger', data);
+		let suggestionResult;
+
+		before(() => {
+			suggestionResult = hestonBot(undefined, 'heston review Krusty Burger', data());
+		});
 
 		it('asks the user to confirm restaurants they want to review', () => {
 			expect(suggestionResult.messages.length).to.equal(2);
@@ -25,7 +29,11 @@ describe('conversation with Heston', () => {
 		});
 
 		describe('conversation if the user confirms the restaurant', () => {
-			const confirmedResult = hestonBot(suggestionResult.state, 'yes', data);
+			let confirmedResult;
+
+			before(() => {
+				confirmedResult = hestonBot(suggestionResult.state, 'yes', data());
+			});
 
 			it('asks for a rating once the user has confirmed the restaurant', () => {
 				expect(confirmedResult.messages.length).to.equal(1);
@@ -33,7 +41,11 @@ describe('conversation with Heston', () => {
 			});
 
 			describe('conversation if the user provides a rating', () => {
-				const ratedResult = hestonBot(confirmedResult.state, '5', data);
+				let ratedResult;
+
+				before(() => {
+					ratedResult = hestonBot(confirmedResult.state, '5', data());
+				});
 
 				it('asks for a description if a rating is provided', () => {
 					expect(ratedResult.messages.length).to.equal(1);
@@ -41,20 +53,38 @@ describe('conversation with Heston', () => {
 				});
 
 				describe('conversation once the user has provided a description', () => {
-					const describedResult = hestonBot(ratedResult.state, 'A++++', data);
+					let describedResult;
+					let reviewData;
+
+					before(() => {
+						describedResult = hestonBot(ratedResult.state, 'A++++', data());
+						reviewData = describedResult.state.reviews[0];
+					});
 
 					it('thanks the user for their review', () => {
 						expect(describedResult.messages.length).to.equal(1);
 						expect(describedResult.messages[0].message).to.equal('Your review has been noted my friend!');
 					});
 
+					it('now has review data permanently available', () => {
+						expect(reviewData).to.deep.equal({
+							description: 'A++++',
+							rating: 5,
+							restaurant: 'Krusty Burger',
+							user: 'Fred',
+							placeInfo: {
+								tripAdvisorLink: '@Krusty Burger'
+							}
+						});
+					});
+
 					it('ignores subsequent random input', () => {
-						const result = hestonBot(describedResult.state, 'Hi', data);
+						const result = hestonBot(describedResult.state, 'Hi', data());
 						expect(result.messages.length).to.equal(0);
 					});
 
 					it('allows a new review to be started', () => {
-						const result = hestonBot(describedResult.state, 'heston review Mo\'s Diner', data);
+						const result = hestonBot(describedResult.state, 'heston review Mo\'s Diner', data());
 						expect(result.messages.length).to.equal(2);
 						expect(result.messages[1].message).to.equal('Is this the restaurant you want to review?');
             return expect(result.messages[0].message).to.eventually.equal('@Mo\'s Diner');
@@ -63,7 +93,11 @@ describe('conversation with Heston', () => {
 			});
 
 			describe('conversation if the user does not provide a rating', () => {
-				const unratedResult = hestonBot(confirmedResult.state, 'Nice day eh?', data);
+				let unratedResult;
+
+				before(() => {
+					unratedResult = hestonBot(confirmedResult.state, 'Nice day eh?', data());
+				});
 
 				it('has a strop if no rating is provided', () => {
 					expect(unratedResult.messages.length).to.equal(1);
@@ -71,14 +105,18 @@ describe('conversation with Heston', () => {
 				});
 
 				it('does not allow the user to subsequently provide a rating once they have failed to', () => {
-					const result = hestonBot(unratedResult.state, '5', data);
+					const result = hestonBot(unratedResult.state, '5', data());
 					expect(result.messages.length).to.equal(0);
 				});
 			});
 		});
 
 		describe('conversation if the user does not confirm the restaurant', () => {
-			const unconfirmedResult = hestonBot(suggestionResult.state, 'no', data);
+			let unconfirmedResult;
+
+			before(() => {
+				unconfirmedResult = hestonBot(suggestionResult.state, 'no', data());
+			});
 
 			it('asks the user to restart the conversation again if the restaurant can not be confirmed', () => {
 				expect(unconfirmedResult.messages.length).to.equal(1);
@@ -86,7 +124,7 @@ describe('conversation with Heston', () => {
 			});
 
 			it('does not allow the user to subsequently say yes and continue once they have said no', () => {
-				const result = hestonBot(unconfirmedResult.state, 'no', data);
+				const result = hestonBot(unconfirmedResult.state, 'no', data());
 				expect(result.messages.length).to.equal(0);
 			});
 		});
